f5d5c2ed6dde851e59f5a5514380d8de6902ef8f
ldhatter <liuaihatter@gmail.com>
2018-04-12 15:51:21 -0400

    #86 Inject message service to errors-handler

--- angular-client/src/app/errors/errors-handler.ts
+++ angular-client/src/app/errors/errors-handler.ts
@@ -1,21 +1,31 @@
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MessageService } from '../services/message.service';
 
 @Injectable()
 export class ErrorsHandler implements ErrorHandler {
 
+    constructor(private injector: Injector) { }
+
     handleError(error: Error | HttpErrorResponse) {
+
+        const messageService = this.injector.get(MessageService);
+
+
         if (error instanceof HttpErrorResponse) {
             // Error: server or connection
             if (!navigator.onLine) {
                 // Error: application offline
-            } else {
-                // Error: Http
+                return messageService.add('No Internet Connection');
             }
+
+            // Error: Http
+            return messageService.add(`${error.status} - ${error.message}`);
+
         } else {
             // Error: Client
+            return messageService.add(`${error}`);
         }
 
-        console.error('Error: ', error);
     }
 }
