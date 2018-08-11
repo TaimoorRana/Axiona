7a80990663719f68693a164e21826f29882316c2
James Moore <freeone3000@gmail.com>
2018-08-11 14:46:24 -0400

    added another var name

--- express-server/models/mongo_url.js
+++ express-server/models/mongo_url.js
@@ -1,5 +1,6 @@
 const MONGO_URL = process.env.MONGO_URL || 
-                  process.env.MONGO_URI || 
+                  process.env.MONGO_URI ||
+                  process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI ||
                   process.env.MONGOLAB_URL ||
                   'mongodb://localhost/axiona';
